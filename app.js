1172cb8f6dd1466e82e41ea18ac3fe66500dd66b
joncancode <jc.haines19@gmail.com>
2017-12-02 16:00:31 -0500

    setting up css borders to show separate sections

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+  //init scrollmagic
+  let controller = new ScrollMagic.Controller();
 
-//init scrollmagic
-let controller = new ScrollMagic.Controller()
+  //build scene
+  let myScene = new ScrollMagic.Scene({
 
 
+  }).setClassToggle('#mainHeader', 'fade-in')
 });
